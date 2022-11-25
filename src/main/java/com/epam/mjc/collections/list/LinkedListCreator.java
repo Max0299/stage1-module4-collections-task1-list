4a826dd451a4b7fe355e0e337471da567cd9d87f
Max <maxageev5@gmail.com>
2022-11-25 16:10:57 +0300

    2

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,21 @@
 package com.epam.mjc.collections.list;
 
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (Integer integer : sourceList) {
+            if (integer % 2 != 0) {
+                linkedList.addFirst(integer);
+            } else {
+                linkedList.addLast(integer);
+            }
+        }
+        return linkedList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        Comparator<String> comparator = new ListComparator();
+
+        sourceList.sort(comparator);
+
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int x = Integer.parseInt(a);
+        int y = Integer.parseInt(b);
+
+        return Integer.compare(5 * x * x - 3, 5 * y * y - 3);
+
     }
 }
